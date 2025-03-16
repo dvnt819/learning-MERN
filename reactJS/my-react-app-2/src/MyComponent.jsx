a4ae2c8ce157edef3f5512ac32779a4d325dd3f5
Dhvanit <dhvanit03@gmail.com>
2025-03-16 20:45:19 +0530

    Counter using useState hook

--- reactJS/my-react-app-2/src/MyComponent.jsx
+++ reactJS/my-react-app-2/src/MyComponent.jsx
@@ -2,32 +2,54 @@ import React,{useState} from "react"
 
 export default function MyComponent(){
 
-    let [name="Guest",setName] = useState();
+    // let [name="Guest",setName] = useState();
 
-    const updateName=()=>{
-        setName("Dhvanit")
+    // const updateName=()=>{
+    //     setName("Dhvanit")
+    // }
+
+    // let [age=0,setAge]=useState();
+
+    // const incrementAge=()=>{
+    //     setAge(age+1)
+    // }
+
+    // let [isEmployed=false,toggleIt]=useState();
+
+    // const toggleFun=()=>{
+    //     toggleIt(!isEmployed)
+    // }
+
+    let [count,setCount]=useState(0)
+
+    const incrementCount = ()=>{
+        setCount(count+1)
     }
 
-    let [age=0,setAge]=useState();
-
-    const incrementAge=()=>{
-        setAge(age+1)
+    const decrementCount = ()=>{
+        setCount(count-1)
     }
 
-    let [isEmployed=false,toggleIt]=useState();
-
-    const toggleFun=()=>{
-        toggleIt(!isEmployed)
+    const resetCount = ()=>{
+        setCount(0)
     }
+
 
     return (
-        <div>
-            <h2>Name : {name}</h2>
+        <div className="container-div">
+            {/* <h2>Name : {name}</h2>
             <button onClick={updateName}>Show My Name</button>
             <h2>Age : {age}</h2>
             <button onClick={incrementAge}>Increment Age</button>
             <h2>is Employed : {isEmployed ? "Yes" : "No"}</h2>
-            <button onClick={toggleFun}>Toggle</button>
+            <button onClick={toggleFun}>Toggle</button> */}
+            <h1>Counter</h1>
+            <p className="counter">{count}</p>
+            <div className="button-div">
+                <button onClick={decrementCount}>Decrement</button>
+                <button onClick={resetCount}>Reset</button>
+                <button onClick={incrementCount}>Increment</button>
+            </div>
         </div>
     )
 }
