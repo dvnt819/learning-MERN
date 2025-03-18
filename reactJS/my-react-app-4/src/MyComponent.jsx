3729bfcb32f12a4a3a5bbe911cd1fe84f3b4a89b
Dhvanit <dhvanit03@gmail.com>
2025-03-18 09:24:06 +0530

    update array in state

--- reactJS/my-react-app-4/src/MyComponent.jsx
+++ reactJS/my-react-app-4/src/MyComponent.jsx
@@ -2,29 +2,47 @@ import React,{useState,onChange} from "react";
 
 export default function(){
 
-    const [car,setCar] = useState({year:2024,make:"Ford",model:"Mustang"})
+    // const [car,setCar] = useState({year:2024,make:"Ford",model:"Mustang"})
 
-    function handleYearChange(event){
-        setCar({...car,year:event.target.value})
+    // function handleYearChange(event){
+    //     setCar({...car,year:event.target.value})
+    // }
+
+    // function handleMakeChange(event){
+    //     setCar({...car,make:event.target.value})
+    // }
+
+    // function handleModelChange(event){
+    //     setCar({...car,model:event.target.value})
+    // }
+
+    const [fruits,setFruits] = useState(["apple","banana","pineapple","orange"])
+    
+    function handleFruitAdd(){
+        const newFruit = document.getElementById("newFruit").value
+        document.getElementById("newFruit").value = ""
+        setFruits(f => [...f,newFruit])
     }
 
-    function handleMakeChange(event){
-        setCar({...car,make:event.target.value})
+    function handleFruitRemove(index){
+        setFruits(fruits.filter((_,i)=>i !== index))
     }
-
-    function handleModelChange(event){
-        setCar({...car,model:event.target.value})
-    }
-
+    
     return (
         <>
-            <p>My favourite car is : {car.year} {car.make} {car.model}</p>
+            {/* <p>My favourite car is : {car.year} {car.make} {car.model}</p>
 
             <input type="number" value={car.year} onChange={handleYearChange} />
             <br />
             <input type="text" value={car.make} onChange={handleMakeChange}/>
             <br />
-            <input type="text" value={car.model} onChange={handleModelChange} />
+            <input type="text" value={car.model} onChange={handleModelChange} /> */}
+
+            <ul>
+                {fruits.map((fruit,index) => <li key={index} onClick={()=>handleFruitRemove(index)}>{fruit}</li>)}
+            </ul>
+            <input type="text" id="newFruit"/>
+            <button onClick={handleFruitAdd}>Add</button>
         </>
     )
 }
